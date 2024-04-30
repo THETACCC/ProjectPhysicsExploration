00ccafadcf8a508c9c4b6fa1bcd0d002784943bf
THETACCC <qingyuanfan03@gmail.com>
2024-04-29 21:34:12 -0400

    restart function

---
+++ Assets/GameLogic/GameRestart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameRestart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        // Load the specified scene by build index
+        SceneManager.LoadScene(1);
+    }
+}
